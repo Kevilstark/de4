73844ea925ac8ea7bd41ca82f3fbc11faf4480fb
TuanHoang <hoanganhtuanpro96@gmail.com>
2021-12-26 19:14:15 +0700

    full websize

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+$( function() {
+    $( ".product-list-inner" ).tabs();
+    $( ".feedback-slider-list" ).slick({
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        arrows: false,
+    });
+} );
+
+
