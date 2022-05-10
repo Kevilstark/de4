99e67dd3cc2b588d73c8db6923383437a99ab545
Kevilstark <hoanganhtuanpro96@gmail.com>
2022-05-10 13:33:49 +0700

    bug fixing

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,11 @@ $( function() {
 } );
 
 
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
